502a48ae8ed4ea1d188bd47bfa5d6bbee2e20584
Miriam Pulgar <miriamconstanzape@gmail.com>
2023-04-10 22:49:00 -0400

    actualizar responsividad

--- js/datatablesClientes.js
+++ js/datatablesClientes.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#mytable').DataTable({
-         "ajax": {
-             "url": "estadisticasjson.json",
-             "dataSrc": ""
-         },
+        responsive: true,
+        "ajax": {
+            "url": "estadisticasjson.json",
+            "dataSrc": ""
+        },
         "columns": [
             { "data": "nombre", className: "text-center" },
             { "data": "apellido", className: "text-center" },
             { "data": "run", className: "text-center" },
             { "data": "nacionalidad", className: "text-center" },
-            { "data": "monto", className: "text-center"},
-            { "data": "cantidad" , className: "text-center"}
+            { "data": "monto", className: "text-center" },
+            { "data": "cantidad", className: "text-center" }
         ]
     });
 });
---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+Fancybox.bind('[data-fancybox]', {
+   
+
+
+  })
+
+
+  *$(document).ready(function () {
+    $("#hidden_link").fancybox().trigger('click')})
+    
+
+  
+/*$(document).ready(function () {
+
+
+    $('a#notebook').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+    $('a#tablet').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+    $('a#camara').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+    $('a#espia').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+    $('a#electrodomestico').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+    $('a#celular').fancybox({
+        overlayShow: true,
+        transitionIn: 'elastic',
+        transitionOut: 'elastic',
+    });
+})*/
